3a28a4bd9c69efcdf01c5e771a5e2e1b82010c93
Anika Kamerkar <kamerkar.an@husky.neu.edu>
2023-03-04 23:42:41 -0500

    a6 React

---
+++ src/labs/a6/conditional-output/conditional-output-if-else.js
@@ -0,0 +1,12 @@
+const ConditionalOutputIfElse = () => {
+    const loggedIn = true;
+    return (
+        <>
+            { loggedIn && <h2>Welcome Inline</h2>}
+            {!loggedIn && <h2>Please login Inline</h2>}
+        </>
+    );
+
+};
+export default ConditionalOutputIfElse;
+
